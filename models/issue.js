d028119e5441fa02028f011555d4482f0f19d889
DRHood <darryl.r.hood@gmail.com>
2020-02-21 12:08:25 -0500

    ironing out the wrinkles

--- models/issue.js
+++ models/issue.js
@@ -1,10 +1,23 @@
 const mongoose = require('../db/connection.js')
 
 const Issue = new mongoose.Schema({
-    description: String,
-    createdAt: Date,
-    status: String,
-    priority: String
+    description: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    status: {
+        type: String,
+        default: 'open',
+    },
+    priority: {
+        type: String,
+        required: true,
+        enum: ['High', 'Medium', 'Low'],
+    },
 });
 
 module.exports = mongoose.model('Issue', Issue);
