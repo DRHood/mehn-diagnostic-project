3bc66a754462a15757a70f5077cdb881bc910f89
DRHood <darryl.r.hood@gmail.com>
2020-02-21 10:12:53 -0500

    made routes(originally forgot that part), various small edits

--- controllers/issues.js
+++ controllers/issues.js
@@ -2,7 +2,7 @@ const express = require('express');
 const issuesRouter = express.Router();
 
 // import APIs
-const Issue = require('../models/Issue.js')
+const Issue = require('../models/Issue.js');
 
 // request handlers
 // get all issues
@@ -12,6 +12,47 @@ issuesRouter.get('/', (req, res) => {
     res.render('issues/issues', { issues });
   });
 });
+
+// post new issue
+issuesRouter.post('/', (req, res) => {
+ Issue.create(req.body).then(() =>
+  {
+      res.redirect('/issues');
+  });
+});
+
+// get new issue form
+issuesRouter.get('/new', (req, res) => {
+  res.render('issues/newIssueForm');
+});
+
+// get issue edit form
+issuesRouter.get('/:id/edit', (req, res) => {
+  Issue.findById(req.params.id).then((issue) => {
+      res.render('issues/editIssueForm', {issue});
+  });
+});
+
+// put issue edit
+issuesRouter.put('/:id', (req, res) => {
+  Issue.findByIdAndUpdate(req.params.id, req.body).then(() => {
+    res.redirect('/issues');
+  });
+});
+
+// get single issue
+issuesRouter.get('/:id', (req, res) => {
+  Issue.findById(req.params.id).then((issue) => {
+      res.render('./issues/issue', {issue});
+  });
+});
+
+// delete issue
+issuesRouter.delete('/:id', (req, res) => {
+  Issue.findByIdAndRemove(req.params.id).then(() => {
+      res.redirect('/issues')
+  })
+})
 
 // router export
 module.exports = issuesRouter;
